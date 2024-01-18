0e4dc989b5b9a822e8c72f72fa020d012d7934cf
benshabbat <benshabbat27@gmail.com>
2024-01-19 01:37:36 +0200

    clean code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ document.onreadystatechange = function (e) {
   if (document.readyState === "complete") {
     console.log("im ready");
     getEmailUsers();
-    createCar();
     getDataForTable();
     render2();
   }
@@ -32,31 +31,11 @@ function render2() {
     });
     //if is add
     if (selctedRow === null) {
-      const list = document.querySelector(".table-users-list");
-      const row = document.createElement("tr");
-      row.innerHTML = `
-      <td>${newUser.ID}</td>
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td>${email}</td>
-        <td>${age}</td>
-        <td>${newUser.car.model}</td>
-        <td><button class="delete">Delete</button>
-        <button class="edit">Edit</button></td>
-        `;
-      list.appendChild(row);
-
-      selctedRow = null;
-      clearFields();
-      DB.users.push(newUser);
-      //if is edit
-    } else {
-      selctedRow.children[1].textContent = firstName;
-      selctedRow.children[2].textContent = lastName;
-      selctedRow.children[3].textContent = email;
-      selctedRow.children[4].textContent = age;
-      selctedRow = null;
-      clearFields();
+      addUser(newUser);
+    }
+    //if is edit
+    else {
+      editUser(newUser);
     }
     console.log(DB.users);
   });
@@ -65,83 +44,50 @@ function render2() {
     target = e.target;
     //Delete data
     if (target.classList.contains("delete")) {
-      console.log("clicked delete");
-      selctedRow = target.parentNode.parentNode;
-      console.log("the id" + selctedRow.children[0].textContent);
-      //remove data from db
-      const newArr = DB.users.filter((user) => {
-        return user.ID != selctedRow.children[0].textContent;
-      });
-      console.log("the new array" + newArr);
-      DB.users = newArr;
-      console.log("the new DB" + DB.users);
-      selctedRow = null;
-      target.parentNode.parentNode.remove();
+      deleteData();
     }
+
     //Edit data
     if (target.classList.contains("edit")) {
-      console.log("clicked edit");
-
-      selctedRow = target.parentNode.parentNode;
-      console.log(selctedRow);
-      idUserEdit = Number(selctedRow.children[0].textContent);
-      document.getElementById("firstName").value =
-        selctedRow.children[1].textContent;
-      document.getElementById("lastName").value =
-        selctedRow.children[2].textContent;
-      document.getElementById("email").value =
-        selctedRow.children[3].textContent;
-      document.getElementById("age").value = selctedRow.children[4].textContent;
-      console.log(idUserEdit);
+      insertToFormEditData();
     }
   });
-  document.querySelector(".table-users-list").addEventListener("click", (e) => {
-    target = e.target;
+
+
+  //Creat Car
+  document.querySelector("#form-cars").addEventListener("submit", (e) => {
+    createCar();
   });
 
-  //get Users
-
-
-
-
-
 }
+
+
+
+
 function clearFields() {
   document.getElementById("firstName").value = "";
   document.getElementById("lastName").value = "";
   document.getElementById("email").value = "";
   document.getElementById("age").value = "";
 }
+function createCar() {
+  e.preventDefault();
+  const idUser = document.getElementById("user-list-car").value;
+  const model = document.getElementById("model").value;
+  const color = document.getElementById("color").value;
+  const year = document.getElementById("year").value;
+  const newCar = new Car(model, color, Number(year));
+  DB.users.forEach((user) => {
+    if (idUser == user.ID) user.car = newCar;
+  });
 
+  DB.cars.push(newCar);
 
-
-
-
-
-
-  //Creat Car
-  function createCar(){
-    document.querySelector("#form-cars").addEventListener("submit", (e) => {
-      e.preventDefault();
-      const idUser = document.getElementById("user-list-car").value;
-      const model = document.getElementById("model").value;
-      const color = document.getElementById("color").value;
-      const year = document.getElementById("year").value;
-      const newCar = new Car(model, color, Number(year));
-      DB.users.forEach((user) => {
-        if (idUser == user.ID) user.car = newCar;
-      });
-  
-      DB.cars.push(newCar);
-     
-      console.log(DB.cars);
-      console.log(DB.users);
-    });
-  
-  }
+  console.log(DB.cars);
+  console.log(DB.users);
+}
 //getEmailUsers
-function getEmailUsers(){
-
+function getEmailUsers() {
   DB.users.forEach((user) => {
     const usersCar = document.querySelector(".usersCar");
     const option = document.createElement("option");
@@ -149,4 +95,61 @@ function getEmailUsers(){
     option.innerHTML = user.email;
     usersCar.appendChild(option);
   });
+}
+
+function addUser() {
+  const list = document.querySelector(".table-users-list");
+  const row = document.createElement("tr");
+  row.innerHTML = `
+  <td>${newUser.ID}</td>
+    <td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${email}</td>
+    <td>${age}</td>
+    <td>${newUser.car.model}</td>
+    <td><button class="delete">Delete</button>
+    <button class="edit">Edit</button></td>
+    `;
+  list.appendChild(row);
+  clearFields();
+  DB.users.push(newUser);
+}
+
+function deleteData() {
+  console.log("clicked delete");
+  selctedRow = target.parentNode.parentNode;
+  console.log("the id" + selctedRow.children[0].textContent);
+  //remove data from db
+  const newArr = DB.users.filter((user) => {
+    return user.ID != selctedRow.children[0].textContent;
+  });
+  console.log("the new array" + newArr);
+  DB.users = newArr;
+  console.log("the new DB" + DB.users);
+  selctedRow = null;
+  target.parentNode.parentNode.remove();
+}
+
+function insertToFormEditData() {
+  console.log("clicked edit");
+
+  selctedRow = target.parentNode.parentNode;
+  console.log(selctedRow);
+  idUserEdit = Number(selctedRow.children[0].textContent);
+  document.getElementById("firstName").value =
+    selctedRow.children[1].textContent;
+  document.getElementById("lastName").value =
+    selctedRow.children[2].textContent;
+  document.getElementById("email").value = selctedRow.children[3].textContent;
+  document.getElementById("age").value = selctedRow.children[4].textContent;
+  console.log(idUserEdit);
+}
+
+function editUser() {
+  selctedRow.children[1].textContent = firstName;
+  selctedRow.children[2].textContent = lastName;
+  selctedRow.children[3].textContent = email;
+  selctedRow.children[4].textContent = age;
+  selctedRow = null;
+  clearFields();
 }
