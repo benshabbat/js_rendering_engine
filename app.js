cea01c956c989dc364320e8f3471679398f1e5b7
benshabbat <benshabbat27@gmail.com>
2024-01-19 03:01:16 +0200

    clean code

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import render from "./render.js";
 document.onreadystatechange = function (e) {
-    if (document.readyState === "complete") {
-      console.log("im ready");
-      render();
-    }}
+  if (document.readyState === "complete") {
+    console.log("im ready");
+    render();
+  }
+};
--- db.js
+++ db.js
@@ -1,23 +1,18 @@
-import {Car,User} from "./class.js"
+import { Car, User } from "./class.js";
 
 //create a car Static
-const car1 = new Car("Siat","white",2022)
-
+const car1 = new Car("Siat", "white", 2022);
 
 //Create users Static
 
-const User1 = new User("David","Benshabbat","benshabbat27@gmail.com",30)
-const User2 = new User("Miriam","Benshabbat","Miriam@gmail.com",28,car1)
-const User3 = new User("Avishag","Benshabbat","Avishag@gmail.com",2)
+const User1 = new User("David", "Benshabbat", "benshabbat27@gmail.com", 30);
+const User2 = new User("Miriam", "Benshabbat", "Miriam@gmail.com", 28, car1);
+const User3 = new User("Avishag", "Benshabbat", "Avishag@gmail.com", 2);
 
+const DB = {
+  users: [User1, User2, User3],
+  cars: [car1],
+  API: {},
+};
 
-const DB={
-    users:[User1, User2, User3],
-    cars:[car1],
-    API:{
-        
-    }
-
-}
-
-export default DB
+export default DB;
