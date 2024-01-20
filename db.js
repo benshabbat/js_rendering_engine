17995e5e2b9154d84de90c39dea528378832d3c8
benshabbat <benshabbat27@gmail.com>
2024-01-20 23:03:50 +0200

    add car to avishag staticc

--- db.js
+++ db.js
@@ -8,11 +8,11 @@ const car2 = new Car("Ferrari", "blue", 2023);
 
 const User1 = new User("David", "Benshabbat", "benshabbat27@gmail.com", 30);
 const User2 = new User("Miriam", "Benshabbat", "Miriam@gmail.com", 28, car1);
-const User3 = new User("Avishag", "Benshabbat", "Avishag@gmail.com", 2);
+const User3 = new User("Avishag", "Benshabbat", "Avishag@gmail.com", 2,car2);
 
 const DB = {
   users: [User1, User2, User3],
-  cars: [car1],
+  cars: [car1,car2],
   API: {},
 };
 
